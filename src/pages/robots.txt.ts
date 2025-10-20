c4f58d357c71ada11544d8685ca5f0cce22f90cb
millioncreative <56916938+millioncreative@users.noreply.github.com>
2025-10-20 16:54:57 +0800

    Fix robots sitemap URL for preview origin

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,11 +1,15 @@
 import type { APIRoute } from 'astro';
+import { withBase } from '../utils/paths';
 import { site as projectSite } from '../../project.config.js';
 
 export const prerender = true;
 
-export const GET: APIRoute = ({ site }) => {
-  const siteUrl = site ?? projectSite;
-  const sitemapUrl = new URL('sitemap.xml', siteUrl).href;
+export const GET: APIRoute = ({ site, request }) => {
+  const requestUrl = new URL(request.url);
+  const origin = requestUrl.origin || site?.origin || new URL(projectSite).origin;
+  const sitemapPath = withBase('sitemap.xml');
+  const sitemapUrl = new URL(sitemapPath, `${origin}/`).href;
+
   return new Response(`User-agent: *\nAllow: /\nSitemap: ${sitemapUrl}\n`, {
     headers: {
       'Content-Type': 'text/plain; charset=utf-8'
